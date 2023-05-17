d1d9a34dd1f1625a75cfbe93708d28f25c9ef4f2
fet1sov <august9222@gmail.com>
2023-05-17 17:21:24 +0500

    💙Profile: Updated settings menu (6)
    — Client: Drag and Drop for avatars. Store file on local machine

--- web/js/settings.js
+++ web/js/settings.js
@@ -1,9 +1,57 @@
+function previewProfilePicture()
+{
+    event.stopPropagation();
+    event.preventDefault();
+    const fileList = event.dataTransfer.files;
+
+    let reader = new FileReader();
+
+    console.log(fileList[0]);
+
+    const profilePic = document.getElementById("smallProfilePic");
+    const profilePic_highRes = document.getElementById("profilePicHighRes");
+    const profilePic_medRes = document.getElementById("profilePicMediumRes");
+    const profilePic_lowRes = document.getElementById("profilePicLowRes");
+
+    if (profilePic)
+    {
+        if (fileList[0].type === "image/png" 
+        || fileList[0].type === "image/jpeg" 
+        || fileList[0].type === "image/gif")
+        {
+            profilePic_highRes.src = URL.createObjectURL(fileList[0]);
+            profilePic_medRes.src = URL.createObjectURL(fileList[0]);
+            profilePic_lowRes.src = URL.createObjectURL(fileList[0]);
+        }
+    }
+}
+
+function loadImage()
+{
+
+}
+
 function saveSettings()
 {
 
 }
 
 window.addEventListener("DOMContentLoaded", (event) => {
+    const loadImageButton = document.getElementById("btnLoadImage");
+    if (loadImageButton)
+    {
+        loadImageButton.addEventListener("click", loadImage, false)
+    }
+
+    const dropArea = document.getElementById('dropImgArea');
+    dropArea.addEventListener('dragover', (event) => {
+        event.stopPropagation();
+        event.preventDefault();
+        event.dataTransfer.dropEffect = 'copy';
+    });
+
+    dropArea.addEventListener('drop', this.previewProfilePicture);
+
     const saveButton = document.getElementById("btn-save");
     if (saveButton)
     {
